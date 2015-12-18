192927b5c57274e5b28ff1cad433f2f0eb8928af
priyankakush <priyanka803972@gmail.com>
2015-12-18 17:08:48 +0530

    added

---
+++ app/src/main/java/com/sourcebits/priyankakushwaha/calendarwithswipe/CalendarViewWithSwipe.java
@@ -0,0 +1,16 @@
+package com.sourcebits.priyankakushwaha.calendarwithswipe;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+
+public class CalendarViewWithSwipe extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main);
+    }
+
+}
